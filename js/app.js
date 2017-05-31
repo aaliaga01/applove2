921006ccd460d31cd59b8118fe288c6824ae3eaf
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-31 00:21:10 -0300

    funciona solo con una img TOT

--- js/app.js
+++ js/app.js
@@ -6,14 +6,14 @@
  "assets/images/img-4.jpg","assets/images/img-5.jpg","assets/images/img-6.jpg",
  "assets/images/img-7.jpg","assets/images/img-8.jpg","assets/images/img-9.jpg"]
  
- galeria.forEach(function(img){
+ galeria.forEach(function(val){
 	
 	var boxWork = document.createElement("div");
 	boxWork.setAttribute("class","box-work");
 	document.querySelector("#work .container").appendChild(boxWork);
 
 	var imagen = document.createElement("img");
-	imagen.setAttribute("src", img);
+	imagen.setAttribute("src", val);
 	imagen.setAttribute("width", "300");
 	boxWork.appendChild(imagen);
 
@@ -21,6 +21,12 @@
 	var proyecto = document.createTextNode("Nombre del proyecto");
 	boxWork.appendChild(titulo);
 	titulo.appendChild(proyecto);
-		}
-);
+
+	const mostrar = document.querySelector("#work .box-work img");
+	mostrar.addEventListener("click", function(){
+		alert("hola mundo")
+	});
+});
+
+
 
