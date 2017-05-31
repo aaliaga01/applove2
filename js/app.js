0d13edb060f0263f2dc9eae2b3a765d3f4a3f2a2
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-30 23:17:50 -0300

    comenzandoa sufrir con js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+ const galeria = ["assets/images/img-1.png", "assets/images/img-2.jpg", "assets/images/img-3.jpg",
+ "assets/images/img-4.jpg","assets/images/img-5.jpg","assets/images/img-6.jpg",
+ "assets/images/img-7.jpg","assets/images/img-8.jpg","assets/images/img-9.jpg"]
