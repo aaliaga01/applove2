239c8e88df8e5416b79a28231395e9ad085ef74c
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-30 23:57:43 -0300

    lista nueva sección work

--- js/app.js
+++ js/app.js
@@ -2,6 +2,25 @@
  * Archivo principal de funcionalidad de JS
  */
 
- const galeria = ["assets/images/img-1.png", "assets/images/img-2.jpg", "assets/images/img-3.jpg",
+ const galeria = ["assets/images/img-1.jpg", "assets/images/img-2.jpg", "assets/images/img-3.jpg",
  "assets/images/img-4.jpg","assets/images/img-5.jpg","assets/images/img-6.jpg",
  "assets/images/img-7.jpg","assets/images/img-8.jpg","assets/images/img-9.jpg"]
+ 
+ galeria.forEach(function(img){
+	
+	var boxWork = document.createElement("div");
+	boxWork.setAttribute("class","box-work");
+	document.querySelector("#work .container").appendChild(boxWork);
+
+	var imagen = document.createElement("img");
+	imagen.setAttribute("src", img);
+	imagen.setAttribute("width", "300");
+	boxWork.appendChild(imagen);
+
+	var titulo = document.createElement("h5");
+	var proyecto = document.createTextNode("Nombre del proyecto");
+	boxWork.appendChild(titulo);
+	titulo.appendChild(proyecto);
+		}
+);
+
